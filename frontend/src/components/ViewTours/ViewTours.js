333124b12c2e98a9ef8740259706788df91b4ec5
Souviksamanta34 <souviksamanta34@.com>
2023-07-29 03:01:32 +0530

    fsfv

--- frontend/src/components/ViewTours/ViewTours.js
+++ frontend/src/components/ViewTours/ViewTours.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import "./ViewTours.css";
 import { Link } from "react-router-dom";
-// Remove ".map" from the end of the import URL
-import "bootstrap/dist/css/bootstrap.min.css";
 const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 class ViewTours extends Component {
@@ -17,7 +15,7 @@ class ViewTours extends Component {
 
   async componentDidMount() {
     try {
-      const response = await fetch(`${apiUrl}/view`);
+      const response = await fetch(`${apiUrl}/view`); // <-- Updated URL here
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -35,60 +33,7 @@ class ViewTours extends Component {
     }
   }
 
-  render() {
-    const { tours, isLoading, error } = this.state;
-
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-
-    if (error) {
-      return <div>{error}</div>;
-    }
-
-    return (
-      <div className="container">
-        <br />
-        <div className="justify-content-center">
-          <h1 className="rr">Tour Packages</h1>
-        </div>
-        <br />
-        <br />
-
-        <div className="row">
-          {tours.map((value, key) => (
-            <div className="col-md-4" key={key}>
-              <div className="card">
-                <img
-                  src="https://i.pinimg.com/originals/54/2c/7e/542c7e0c990d70f3e732b8a9cf64815f.png"
-                  className="card-img-top"
-                  alt="Tour Package"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{value.tname}</h5>
-                  <p className="card-text justify">{value.tdescription}</p>
-                  <br />
-                  <p className="card-text mb-0 p-8">
-                    <b>Price: </b>
-                    {value.price}
-                  </p>
-                  <p className="card-text">
-                    <b>Package No: </b>
-                    {value.tnumber}
-                  </p>
-                  <div className="text-center">
-                    <Link to="/add" className="btn btn-primary">
-                      Book Now
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+  // ... rest of the component code remains unchanged
 }
 
 export default ViewTours;
